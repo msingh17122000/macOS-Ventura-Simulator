78f392059fe98c3838d96d6ea5859f9fa197456b
msingh17122000 <66904586+msingh17122000@users.noreply.github.com>
2023-09-14 17:42:24 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,141 @@
+dragElement(document.getElementById("openTab"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("wallpaperTab"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+function openTab(){
+    var tab=document.getElementById("openTab");
+        if(tab.style.display=="none"){
+            tab.style.animation='fadeIn 0.3s ease';
+            setTimeout(()=>{tab.style.display='block'}, 300);
+        }
+        else{
+          tab.style.animation='fadeIn 0.3s ease';
+          setTimeout(()=>{tab.style.display='block'}, 300);
+        }
+}
+function openWallpaperTab(){
+  var tab=document.getElementById("wallpaperTab");
+      if(tab.style.display=="none"){
+          tab.style.animation='fadeIn 0.3s ease';
+          setTimeout(()=>{tab.style.display='block'}, 300);
+      }
+      else{
+        tab.style.animation='fadeIn 0.3s ease';
+        setTimeout(()=>{tab.style.display='block'}, 300);
+      }
+}
+
+function closeTab(){
+    var tab=document.getElementById("openTab");
+    tab.style.animation="fadeOut 0.3s";
+    setTimeout(()=>{tab.style.display='none'}, 300);
+}
+function maximizeTab(){
+    var tab=document.getElementById("openTab");
+    tab.classList.toggle("maximizeTab");  
+}
+
+var loadingScreen=document.getElementById("loadingPage");
+setTimeout(()=>{loadingScreen.style.display='none'}, 5000);
+
+
+function ChangeWall(el){
+ console.log(el.src);
+ var image=el.src;
+ var body=document.getElementById("root");
+ body.style.backgroundImage="url("+image+")";
+}
+
+function closeTabWall(){
+  var tab=document.getElementById("wallpaperTab");
+  tab.style.animation="fadeOut 0.3s";
+  setTimeout(()=>{tab.style.display='none'}, 300);
+}
+function maximizeTabWall(){
+  var tab=document.getElementById("wallpaperTab");
+  tab.classList.toggle("maximizeTab");  
+}
